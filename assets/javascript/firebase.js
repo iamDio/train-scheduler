23a7bd347aa7d2de44ffadb1d07c2f41e35ec58e
iamDio <claudio.villani00@gmail.com>
2017-05-27 15:12:15 -0400

    debugging firebase error

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,19 +1,14 @@
-      // Initialize Firebase
-var config = {
+
+<script src="https://www.gstatic.com/firebasejs/4.0.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyC5x_AlcmMJO-E9a2O6HCCaTaDWhZOgZq4",
     authDomain: "train-scheduler-9e284.firebaseapp.com",
     databaseURL: "https://train-scheduler-9e284.firebaseio.com",
     projectId: "train-scheduler-9e284",
     storageBucket: "train-scheduler-9e284.appspot.com",
     messagingSenderId: "488098087514"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-var clickcounter = 0;
-
-$('#test').on('click', function(){
-  clickcounter++;
-  database.ref().set({ clickcounter});
-});
+  };
+  firebase.initializeApp(config);
+</script>
